ff4515893f3c1692d706cfe125e66b7ae714a084
Micah Martin <micah.martin@droneup.com>
2024-08-27 20:14:36 -0700

    adding src path and dir with deps.cljs in src

--- deps.cljs
+++
@@ -1,5 +0,0 @@
-{:foreign-libs
- [{:file "jsts/jsts.min.js",
-   :provides ["com.cleancoders.cljsts"],
-   :file-min "jsts/jsts.min.js"}],
- :externs ["jsts/jsts.ext.js"]}
---
+++ src/deps.cljs
@@ -0,0 +1,5 @@
+{:foreign-libs
+ [{:file "src/src.min.js",
+   :provides ["com.cleancoders.cljsts"],
+   :file-min "src/src.min.js"}],
+ :externs ["src/src.ext.js"]}
